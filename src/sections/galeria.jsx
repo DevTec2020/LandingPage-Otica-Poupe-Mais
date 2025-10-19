3fbd59cc31924f5e74643fe27bd5b4d0100196ce
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-10-19 12:43:48 -0300

    feat: adicionando imagens

--- src/sections/galeria.jsx
+++ src/sections/galeria.jsx
@@ -3,20 +3,20 @@ import person2 from "../assets/Clientes/person2.webp"
 import person3 from "../assets/Clientes/person3.webp"
 import person4 from "../assets/Clientes/person4.webp"
 
+
 export default function Galeria(){
     const GALLERY_IMAGE_URLS = [
     person1,
     person2,
     person3,
     person4,
-    // Adicione mais imagens conforme necessário
   ];
 
   return (
     <section className="py-16 px-4 bg-white text-center">
-      <h3 className="text-3xl md:text-4xl font-extrabold mb-10 text-gray-800">
+      <h3 className="text-3xl md:text-3xl font-extrabold mb-10 text-gray-500 uppercase">
         QUEM ENTENDE DE IMAGEM ESCOLHE A{" "}
-        <span className="text-green-600 uppercase">Ótica Poupe Mais</span>
+        <span className="text-brand-secondary uppercase">Ótica Poupe Mais</span>
       </h3>
 
       <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-4">
@@ -28,7 +28,7 @@ export default function Galeria(){
             <img
               src={url}
               alt={`Pessoa satisfeita ${index + 1}`}
-              className="w-full h-full object-cover aspect-square"
+              className="w-[45vh] h-[25vh] object-cover aspect-square"
               loading="lazy"
             />
           </div>
